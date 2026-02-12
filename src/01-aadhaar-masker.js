652849404f6f6c2a237b2e88abfeaaa80914b5b9
viraljain7 <veerljain1234@gmail.com>
2026-02-13 02:15:16 +0530

    solve aadhaar mask 01

--- src/01-aadhaar-masker.js
+++ src/01-aadhaar-masker.js
@@ -29,4 +29,9 @@
  */
 export function maskAadhaar(aadhaarNumber) {
   // Your code here
+
+  if(typeof aadhaarNumber!=="string"||aadhaarNumber.length!==12||!/^[0-9]{12}$/.test(aadhaarNumber))return "INVALID"
+  return `XXXX-XXXX-${aadhaarNumber.slice(8,13)}`
+
 }
+
